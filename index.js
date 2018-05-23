7663a4895af8254f81ab70c77ab98deb7c9e6150
mariofaske <mario.faske@smail.th-koeln.de>
2018-05-23 14:04:59 +0200

    implemented issue #1

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+require('body-parser-xml')(bodyParser);
 
 let app = express();
+
+// We store the data in this variable
+var ordersXML;
+
+//Parse the XML data from the body
+app.use(bodyParser.xml());
 
 // Error Handling
 app.use((err, req, res, next) =>{
     console.error(err.stack);
-    res.end(err.status);
-    next();
+    res.sendStatus(500);
 });
 
 /*
@@ -14,8 +21,17 @@ implements HTTP Verb POST
 respond is the URI of the order 
 with a random integer from 0-9 as orderId
 */
-app.post('/order', (req, res) =>{
-    res.json({ uri: req.protocol + "://" + req.headers.host + "/" + "order" + "/" + Math.floor(Math.random() * 10) });
+app.post('/order', (req, res, next) =>{
+
+    extractOrderFromRequest(req).then((order)=>{
+            if(order == null){
+                res.sendStatus(400);
+            }else{
+                let internalOrderId = saveOrder(order);
+                res.set("Location", computeLocationHeader(req, internalOrderId));
+                res.sendStatus(201);
+            }
+        });
   })
 
 /*
@@ -55,5 +71,40 @@ saved port in a constant variable
 const settings = {
     port: process.env.PORT || 3000
 };
+
+/*
+Building the location header
+*/
+function computeLocationHeader(req, orderId){
+
+    let locationHeader = `${req.protocol}://${req.headers.host}/order/${orderId}`;
+
+    return locationHeader
+}
+
+/*
+This function writes the order into a database under a random ID
+For example purposes we overwrite the old order with the new one
+We dont link the order directly with an ID, but generating a new ID every time
+*/
+function saveOrder(order) {
+
+    let internalOrderId = Math.floor(Math.random() * 10);
+
+    ordersXML = order;
+
+    return internalOrderId;
+}
+
+/*
+Extracting the order from the request body
+*/
+function extractOrderFromRequest(req) {
+    return new Promise((resolve, reject) => {
+        resolve(req.body);
+        reject("");
+    })
+}
+
 
 app.listen(3000);
